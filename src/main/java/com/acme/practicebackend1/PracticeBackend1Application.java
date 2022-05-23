c8bc2891e4206eaeff9deff934cda5fe09eb8018
2CTO <u201521691@upc.edu.pe>
2022-05-23 11:52:28 -0500

    feat: configuring AuditModel

--- src/main/java/com/acme/practicebackend1/PracticeBackend1Application.java
+++ src/main/java/com/acme/practicebackend1/PracticeBackend1Application.java
@@ -2,8 +2,10 @@ package com.acme.practicebackend1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PracticeBackend1Application {
 
     public static void main(String[] args) {
